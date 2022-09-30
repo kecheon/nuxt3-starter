c331431262b9e147eab9d6386eb08ae8b439348b
kecheon <kecheon@gmail.com>
2022-09-30 14:28:28 +0900

    feat: deploy to firebase

--- components/Page/Navbar.vue
+++ components/Page/Navbar.vue
@@ -20,7 +20,11 @@ const menus = computed((): IMenuItem[] => [
   { type: 'link', text: t('pages.test.nav'), route: { name: 'test' } },
   { type: 'link', text: t('pages.post.nav'), route: { name: 'post' } },
   { type: 'link', text: t('pages.setting.nav'), route: { name: 'setting' } },
-  { type: 'link', text: 'Exchanges', route: { name: 'exchanges' } },
+  {
+    type: 'link',
+    text: t('pages.exchanges.nav'),
+    route: { name: 'exchanges' },
+  },
   {
     type: 'button',
     text: t('pages.dashboard.nav'),
